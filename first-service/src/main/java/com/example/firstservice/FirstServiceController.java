56b26d528a5c78e162580932a9143fc2890fa1b1
since1909 <since1909@daum.net>
2024-01-08 09:56:48 +0900

    Fix: random port and load balancing

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,16 +1,27 @@
 package com.example.firstservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController {
+
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the First service.";
@@ -23,7 +34,10 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from First Service.";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+
+
+        return String.format("Hi, there. This is a message from First Service on PORT %s", env.getProperty("local.server.port"));
     }
 }
